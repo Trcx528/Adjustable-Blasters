32a0125f59b83c4f369ef587412b00164a5ac748
Jeff Piquette <Jeff.Piquette@metaltek.com>
2015-08-25 17:35:27 -0500

    now spawns arrows, automatically reloads, durability bars

--- src/main/java/com/trcx/ab/Items/GunBase.java
+++ src/main/java/com/trcx/ab/Items/GunBase.java
@@ -5,7 +5,9 @@ import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.projectile.EntityArrow;
 import net.minecraft.entity.projectile.EntitySnowball;
+import net.minecraft.init.Items;
 import net.minecraft.item.EnumAction;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -50,12 +52,11 @@ public abstract class GunBase extends Item{
         setUnlocalizedName(name);
         setTextureName("AB:" + name);
         setMaxStackSize(1);
+    }
 
-        Map<String, Integer> defaults = new HashMap<String, Integer>();
-        defaults.put(gunData.nbtRounds, 50);
-
+    public GunBase setDefaults(Map<String, Integer> defaults) {
         this.gun = new gunData(defaults);
-
+        return this;
     }
 
     protected boolean canShoot(ItemStack stack){
@@ -64,31 +65,78 @@ public abstract class GunBase extends Item{
     }
 
     @Override
-    public void onUpdate(ItemStack stack, World world, Entity player, int count, boolean isUsing) {
-        super.onUpdate(stack, world, player, count, isUsing);
-        gunData.data gundata = this.gun.load(stack);
+    public double getDurabilityForDisplay(ItemStack stack) {
+        gunData.data gun = this.gun.load(stack);
+        double retval = 0D;
+        if (gun.getReloading()) {
+            retval = (double)(this.reloadTime - gun.getCooldown()) / (double)this.reloadTime;
+        } else {
+            retval = (double) gun.getRounds() / (double) this.clipSize;
+        }
+        //System.out.println(retval);
+        return 1D-retval;
+    }
 
-        if (gundata.getCooldown() > 0) {
-            gundata.setCooldown(gundata.getCooldown() -1);
-        } else if (gundata.getRounds() <= 0 && ! gundata.getReloading()) {
-            gundata.setReloading(true);
-            gundata.setCooldown(this.reloadTime);
-        } else if (gundata.getReloading() && gundata.getClips() > 0) {
-            gundata.setClips(gundata.getClips() -1);
-            gundata.setRounds(this.clipSize);
-            System.out.println("Reloaded...");
+    @Override
+    public void onUsingTick(ItemStack stack, EntityPlayer player, int count) {
+        if (!player.worldObj.isRemote) {
+            System.out.println("Using! " + count);
+            gunData.data gundata = this.gun.load(stack);
+            if (this.canShoot(stack)) {
+                EntityArrow arrow = new EntityArrow(player.worldObj, player, 4f);
+                arrow.setIsCritical(true);
+                arrow.canBePickedUp = 2;
+                player.worldObj.spawnEntityInWorld(arrow);
+                gundata.setCooldown(this.fireRate);
+                gundata.setRounds(gundata.getRounds() - 1);
+            }
         }
     }
 
     @Override
-    public ItemStack onItemRightClick(ItemStack stack, World world, EntityPlayer player) {
+    public void onUpdate(ItemStack stack, World world, Entity player, int param4, boolean param5) {
+        super.onUpdate(stack, world, player, param4, param5);
         gunData.data gundata = this.gun.load(stack);
-        if (this.canShoot(stack)) {
-            world.spawnEntityInWorld(new EntitySnowball(world, player));
-            gundata.setCooldown(this.fireRate);
-            gundata.setRounds(gundata.getRounds() -1);
+        if (gundata.getCooldown() > 0) {
+            if ((gundata.getReloading() && gundata.getClips() > 1) || !gundata.getReloading() ) {
+                gundata.setCooldown(gundata.getCooldown() - 1);
+            }
+        } else if (gundata.getRounds() <= 0 && ! gundata.getReloading()) {
+            gundata.setReloading(true);
+            gundata.setCooldown(this.reloadTime);
+        } else if (gundata.getReloading() && gundata.getClips() > 0) { // and cooldown is 0
+            gundata.setClips(gundata.getClips() -1);
+            gundata.setRounds(this.clipSize);
+            gundata.setReloading(false);
+            System.out.println("Reloaded...");
+
         }
+    }
+
+    @Override
+    public EnumAction getItemUseAction(ItemStack stack) {
+        return EnumAction.bow;
+    }
+
+    @Override
+    public int getMaxItemUseDuration(ItemStack stack) {
+        return 72000;
+    }
+
+    @Override
+    public boolean showDurabilityBar(ItemStack stack) {
+        return true;
+    }
+
+    @Override
+    public ItemStack onItemRightClick(ItemStack stack, World world, EntityPlayer player) {
+        System.out.println("onItemRightClick");
+        player.setItemInUse(stack, getMaxItemUseDuration(stack));
         return stack;
     }
 
+    @Override
+    public void onPlayerStoppedUsing(ItemStack stack, World world, EntityPlayer player, int duration) {
+        super.onPlayerStoppedUsing(stack, world, player, duration);
+    }
 }
--- src/main/java/com/trcx/ab/Items/Rifle.java
+++ src/main/java/com/trcx/ab/Items/Rifle.java
@@ -1,14 +1,27 @@
 package com.trcx.ab.Items;
 
+import com.trcx.ab.gunData;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Rifle extends GunBase {
 
     public Rifle (){
         super("Rifle");
-        this.clipSize = 50;
+
+        this.clipSize = 999999999; //TODO Undo set
         this.projectialDamage = 4;
         this.projectialLength = 1;
         this.maxClips = 4;
-        this.reloadTime = 20;
-        this.fireRate = 4;
+        this.reloadTime = 40;
+        this.fireRate = 1;
+
+        Map<String, Integer> defaults = new HashMap<String, Integer>();
+        defaults.put(gunData.nbtRounds, this.clipSize);
+        defaults.put(gunData.nbtEnergy, 10000);
+        defaults.put(gunData.nbtClips, 4);
+        this.setDefaults(defaults);
+
     }
 }
