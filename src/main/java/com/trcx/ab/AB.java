d453e046fc22a6bdafd034d0b29d9688b6e2ee5e
Trcx <Trcx528@gmail.com>
2015-08-24 20:05:11 -0500

    just some boiler plate stuff

---
+++ src/main/java/com/trcx/ab/AB.java
@@ -0,0 +1,6 @@
+package com.trcx.ab;
+
+public class AB {
+    public static final String MODID = "Adjustable Blasters";
+    public static final String VERSION = "0.0";
+}
---
+++ src/main/java/com/trcx/ab/Main.java
@@ -0,0 +1,14 @@
+package com.trcx.ab;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = AB.MODID, version = AB.VERSION)
+public class Main {
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+
+    }
+}
